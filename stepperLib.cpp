fb7c904a5b0809d8595966f6b9b2c269396bb81b
BuildTools <unconfigured@null.spigotmc.org>
2019-04-22 18:46:14 +0200

    All useful library functions should be implemented.

--- stepperLib.cpp
+++ stepperLib.cpp
@@ -1,24 +1,25 @@
 #include <stepperLib.h>
-
+//if higher precision is needed, HALF4WIRE may be able to do half steps.
 AccelStepper stepperL(AccelStepper::FULL4WIRE, 22, 24, 26, 28);
 AccelStepper stepperR(AccelStepper::FULL4WIRE, 48, 46, 44, 42);
 
 //set target position
 void Stepper::setStepTarget(int stepPosL, int stepPosR)
 {  
-    int distL = stepPosL;
-    if (distL < 0) distL=distL*-1;  //distance must be positive
-    stepperL.setMaxSpeed(Stepper::maxSpeed);
-    stepperL.setAcceleration(Stepper::accelconst/(distL/100*distL/100)); //set acceleration in relation to distance
-    //set movement target position
-    stepperL.moveTo(stepPosL);
+  int distL = stepPosL;
+  if (distL < 0) distL=distL*-1;  //distance must be positive
+  stepperL.setMaxSpeed(Stepper::maxSpeed);
+  stepperL.setAcceleration(Stepper::accelconst/(distL/100.0*distL/100.0)); //set acceleration in relation to distance
+  //set movement target position
+  stepperL.moveTo(stepPosL);
 
-    int distR = stepPosR;
-    if (distR < 0) distR=distR*-1;  //distance must be positive
-    stepperR.setMaxSpeed(Stepper::maxSpeed);
-    stepperR.setAcceleration(Stepper::accelconst/(distR/100*distR/100)); //set acceleration in relation to distance
-    //set movement target position
-    stepperR.moveTo(stepPosR);
+  int distR = stepPosR;
+  if (distR < 0) distR=distR*-1;  //distance must be positive
+  stepperR.setMaxSpeed(Stepper::maxSpeed);
+  stepperR.setAcceleration(Stepper::accelconst/(distR/100.0*distR/100.0)); //set acceleration in relation to distance
+  //set movement target position
+  stepperR.moveTo(stepPosR);
+  return;
 }
 
 //move to chosen target
@@ -29,7 +30,7 @@ void Stepper::moveToTarget()
   stepperL.enableOutputs();
   
   //move to position
-  while (stepperL.distanceToGo() != 0 && stepperR.distanceToGo() != 0)
+  while (stepperL.distanceToGo() != 0 && stepperR.distanceToGo() != 0) //.isRunning may fit better
   {
     //move towards position with chosen acceleration and speed
     stepperL.run();
@@ -39,4 +40,22 @@ void Stepper::moveToTarget()
   //disable stepper current
   stepperR.disableOutputs();
   stepperL.disableOutputs();
+  return;
+}
+
+//Set the position the stepper driver thinks it is in. UNTESTED
+void Stepper::setCurrentStepPos(int stepPosL, int stepPosR)
+{
+  stepperL.setCurrentPosition(stepPosL);
+  stepperR.setCurrentPosition(stepPosR);
+  return;
+}
+
+//Get the position the stepper driver thinks it is in. UNTESTED
+int *Stepper::currentStepPos()
+{
+  int pos[2];
+  pos[0] = stepperL.currentPosition();
+  pos[1] = stepperR.currentPosition();
+  return pos;
 }
--- stepperLib.h
+++ stepperLib.h
@@ -2,9 +2,12 @@
 class Stepper
 {
 	private:
-	unsigned int maxSpeed = 10000;
+	unsigned int maxSpeed = 1500; //Documentation says max is about 4000 for one stepper
 	unsigned int accelconst = 100;
 	public:
 	void setStepTarget(int stepPosL, int stepPosR);
 	void moveToTarget();
+	void setCurrentStepPos(int stepPosL, int stepPosR);
+	int *Stepper::currentStepPos();
+	void Stepper::setCurrentStepPos(int stepPosL, int stepPosR);
 };

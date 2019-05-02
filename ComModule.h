329e0ae2dcb988b344f98db2adfbffcb71db1513
magborresen <magge4@yahoo.dk>
2019-05-02 13:21:50 +0200

    added commodule

---
+++ ComModule.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class ComModule
+{
+private:
+    /* data */
+    std::string GCODES [5];
+    bool controlState;
+    float lastPos [2];
+    float currentPos [2];
+    float nextPos [2];
+
+public:
+    ComModule(/* args */);
+    ~ComModule();
+    bool sendCode();
+    bool connect();
+};
+
+
